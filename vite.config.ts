0378e51a154805c12906200e6aeccccd277ee117
Cheval-R <rerisitdikov@gmail.com>
2026-05-08 11:48:02 +0300

    Добавил  base в vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/agro-scanner-2/',
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Bind to all available network interfaces
+    host: '0.0.0.0', // Bind to all available network interfaces
     port: 8080, // Default port, change if necessary
   },
 });
